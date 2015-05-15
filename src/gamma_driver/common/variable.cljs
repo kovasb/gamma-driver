d308c2cd7e5f4be4c8eed80a53fffce96f755587
kovasb <kovas.boguta@gmail.com>
2015-05-15 12:15:37 -0400

    attempt to fix boolean uniforms

--- src/gamma_driver/common/variable.cljs
+++ src/gamma_driver/common/variable.cljs
@@ -45,10 +45,10 @@
         type (:type uniform)
         data (:data input)]
     (case type
-      :bool (.uniform1fv gl location data)
-      :bvec2 (.uniform2fv gl location data)
-      :bvec3 (.uniform3fv gl location data)
-      :bvec4 (.uniform4fv gl location data)
+      :bool (.uniform1iv gl location data)
+      :bvec2 (.uniform2iv gl location data)
+      :bvec3 (.uniform3iv gl location data)
+      :bvec4 (.uniform4iv gl location data)
       :float (.uniform1fv gl location data)
       :vec2 (.uniform2fv gl location data)
       :vec3 (.uniform3fv gl location data)
