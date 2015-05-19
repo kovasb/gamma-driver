328779b4eec5ccb73bd23807b14b325646c1b1cf
kovasb <kovas.boguta@gmail.com>
2015-05-18 23:36:26 -0400

    remove file

--- test/gamma_driver/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns gamma-webgl-driver.core-test
-  (:require [clojure.test :refer :all]
-            [gamma-webgl-driver.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
