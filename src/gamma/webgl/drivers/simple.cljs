bb01ced19a847378274f8540f0a94d2ad3a22f7d
kovasb <kovas.boguta@gmail.com>
2015-09-08 11:33:12 -0400

    WIP

--- src/gamma/webgl/drivers/simple.cljs
+++ src/gamma/webgl/drivers/simple.cljs
@@ -73,6 +73,7 @@
  :buffer-state x
  :operations
  {op {:cached true}}
+ :texture-units {0 tex}
 
  }
 
@@ -106,7 +107,7 @@
     (swap!
       model
       assoc-in
-      [:shaders (:id p) :variable-locations]
+      [:programs (:id p) :variable-locations]
       (variable-locations gl p))))
 
 (defn useProgram [model p]
@@ -122,34 +123,102 @@
            (.useProgram gl po)
            (swap! model assoc :current-program p)))))))
 
+
+(defn createFramebuffer [model fb])
+
 (defn currentFramebuffer [model fb]
   (let [m @model
         gl (:gl m)]
     (if (not= (m :bound-framebuffer))
-      (if fb
-        (if-let [fbo (get-in m [:objects (:id fb)])]
-          (do
-            (.bindFramebuffer gl nil)
-            
-            )
+      (let [fbo (if (nil? fb)
+                  nil
+                  (or
+                    (get-in m [:objects (:id fb)])
+                    (createFramebuffer model fb)))]
+        (.bindFramebuffer gl fbo)
+        (swap! model assoc :bound-framebuffer fb)))))
 
-          )
-        (.bindFramebuffer gl nil)
-        )
+(defn bind-attribute [location buffer layout])
 
-      )
+(defn bindAttributes [model p a]
+  (let [m @model]
+    (when (not= a (get-in m [:programs (:id p) :attributes]))
+      (let [l (get-in m [:programs (:id p) :variable-locations])]
+        (kv-reduce
+          (fn [_ k v]
+            (bind-attribute
+              (l k)
+              ;; assume buffer already exists
+              (get-in m [:objects (:id (:buffer v))])
+              (:layout v)))
+          a)
+        (swap! model assoc-in [:programs (:id p) :attributes] a)))))
+
+
+(defn create-texture [model spec]
+  (let [m @model
+        gl (m :gl)
+        to (.createTexture gl)]
+    (.activeTexture gl (:texture-unit spec))
+    (.bindTexture gl (constant (:target (:texture spec))) to)
+    (swap! model assoc-in [:texture-units texture-unit] texture)
+    
+
+
 
     )
 
   )
 
 
+(defn run-textureData [model op input]
+  (let [gl (@model :gl)]
+
+
+
+    )
+
+  )
+
+
+
+(defn bind-texture-uniform [model location {:keys [texture texture-unit]}]
+  (let [to (or
+             (get-in @model [:objects (:id texture)])
+             (createTexture model tex))]
+    (if (not= texture (get-in @model [:texture-units texture-unit]))
+      (do
+        (.activeTexture gl texture-unit)
+        (.bindTexture gl texture)
+        (swap! model assoc-in [:texture-units texture-unit] texture)))
+    (.uniform1i location texture-unit)))
+
+
+(defn bindTextures [model p u]
+  (let [m @model]
+    ;; assume no one will bind another texture to same texture unit
+    (when (not= u (get-in m [:programs (:id p) :textures]))
+      (let [l (get-in m [:programs (:id p) :variable-locations])]
+        (kv-reduce
+          (fn [_ k v]
+            (bind-texture-uniform model (l k) v))
+          u)
+        (swap! model assoc-in [:programs (:id p) :textures] u)))
+
+
+    )
+
+  )
+
+(defn bindUniforms [model p uniform-inputs])
+
 (defn run-drawArrays [model op input]
   (useProgram model (:program op))
   (currentFramebuffer model (:framebuffer op))
-  (bindAttributes model (:attributes op))
+  (bindAttributes model (:program op) (:attributes op))
   (bindTextures model (:textures op))
   (bindUniforms model p (:uniforms input))
+  (drawArrays model x)
   )
 
 
